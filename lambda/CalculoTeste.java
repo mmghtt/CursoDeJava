fa1e2fdbb8732a53aee362aa787ddba120fddbd2
mi mghtt <48192026+mmghtt@users.noreply.github.com>
2021-03-20 17:45:42 -0300

    CursoDeJava

---
+++ lambda/CalculoTeste.java
@@ -0,0 +1,15 @@
+package lambda;
+
+public class CalculoTeste {
+	public static void main(String[] args) {
+		
+	Calculo calculo = new Soma();
+	System.out.println(calculo.executar(2,  3));
+	
+	calculo = new Multiplicar();
+	System.out.println(calculo.executar(2,  3));
+	
+		
+		
+	}
+}
